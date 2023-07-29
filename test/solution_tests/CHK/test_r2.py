d5b566ccfdb7ad38f2437f80bcdab2c49dde0f94
sbx_user1051 <sbx_user1051@169.254.95.153>
2023-07-29 21:34:17 +0000

    Sat Jul 29 21:34:17 UTC 2023

--- test/solution_tests/CHK/test_r2.py
+++ test/solution_tests/CHK/test_r2.py
@@ -13,8 +13,11 @@ class TestCheckout():
             ("AAABCDF", -1),
             ("EEB", 80),
             ("EEBB", 95),
-            ("EEEEBB", 145),
+            ("EEEEBB", 160),
+            ("BEBEEE", 160),
+            ("")
             ("AAAAAEEB", 280),
         ])
     def test_checkout(self, skus, expected_value):
         assert checkout(skus) == expected_value
+
