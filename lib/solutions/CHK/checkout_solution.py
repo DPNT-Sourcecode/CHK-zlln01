59af56962a5538f0f6ac3cec82e14f4acf1db7ff
sbx_user1051 <sbx_user1051@169.254.95.153>
2023-07-29 20:08:29 +0000

    Sat Jul 29 20:08:29 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -17,4 +17,17 @@ def checkout(skus):
 
     total = 0
 
-    for sku, sku_count in 
+    for sku, sku_count in sku_count.items():
+        # Check if SKU in price table
+        if price_table.get(sku, None) is None:
+            return -1
+
+        # Check for special offers
+        if price_table[sku].get("special_offers", None):
+            if price_table[sku]["special_offers"]["count"] == sku_count:
+                total += price_table[sku]["special_offers"]["price"]
+                continue
+
+        total += (sku * sku_count)
+
+    return total
